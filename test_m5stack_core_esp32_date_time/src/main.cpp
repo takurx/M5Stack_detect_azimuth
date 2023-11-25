0ec905d502ff8e1b9e974a6caef254f2d65c5509
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2023-11-25 16:46:07 +0900

    fail, try PCF8563, code for HYM8563/M5Unit-RTC
    fail, try PCF8563, code for HYM8563/M5Unit-RTC

--- test_m5stack_core_esp32_date_time/src/main.cpp
+++ test_m5stack_core_esp32_date_time/src/main.cpp
@@ -7,28 +7,33 @@ https://nouka-it.hatenablog.com/entry/2022/02/27/133549
 https://registry.platformio.org/libraries/m5stack/M5Stack/installation
 // 3. M5CoreS3/examples/Basics/RTC/RTC_Date_Time/RTC_Date_Time.ino at main · m5stack/M5CoreS3
 https://github.com/m5stack/M5CoreS3/blob/main/examples/Basics/RTC/RTC_Date_Time/RTC_Date_Time.ino
+// 4. Examples · m5stack/M5Unit-RTC – PlatformIO Registry
+https://registry.platformio.org/libraries/m5stack/M5Unit-RTC/examples/Unit_RTC_M5Core/Unit_RTC_M5Core.ino
+
 */
 
 #include <Arduino.h>
 #include <M5Stack.h>
-#include <M5CoreS3.h>
+#include <Unit_RTC.h>
 
-RTC_TimeTypeDef RTCtime;
-RTC_DateTypeDef RTCDate;
+Unit_RTC RTC;
 
-char timeStrbuff[64];
+rtc_time_type RTCtime;
+rtc_date_type RTCdate;
+
+char str_buffer[64];
 
 void flushTime() {
-    M5.Rtc.GetTime(&RTCtime);  // Gets the time in the real-time clock.
-    M5.Rtc.GetDate(&RTCDate);
-    sprintf(timeStrbuff, "%d/%02d/%02d %02d:%02d:%02d", RTCDate.Year,
-            RTCDate.Month, RTCDate.Date, RTCtime.Hours, RTCtime.Minutes,
+    RTC.getTime(&RTCtime);  // Gets the time in the real-time clock.
+    RTC.getDate(&RTCdate);
+    sprintf(str_buffer, "%d/%02d/%02d %02d:%02d:%02d", RTCdate.Year,
+            RTCdate.Month, RTCdate.Date, RTCtime.Hours, RTCtime.Minutes,
             RTCtime.Seconds);
     // Stores real-time time and date data
     // to timeStrbuff.
     M5.Lcd.setCursor(40, 100);
     // Move the cursor position to (x,y).
-    M5.Lcd.println(timeStrbuff);
+    M5.Lcd.println(str_buffer);
     // Output the contents of.
 }
 
@@ -36,26 +41,24 @@ void setupTime() {
     RTCtime.Hours   = 0;  // Set the time.
     RTCtime.Minutes = 40;
     RTCtime.Seconds = 20;
-    if (!M5.Rtc.SetTime(&RTCtime)) Serial.println("wrong time set!");
+    if (!RTC.setTime(&RTCtime)) Serial.println("wrong time set!");
     // and writes the set time to the real time clock.
-    RTCDate.Year  = 2023;  // Set the date.
-    RTCDate.Month = 8;
-    RTCDate.Date  = 28;
-    if (!M5.Rtc.SetDate(&RTCDate)) Serial.println("wrong date set!");
+    RTCdate.Year  = 2023;  // Set the date.
+    RTCdate.Month = 8;
+    RTCdate.Date  = 28;
+    if (!RTC.setDate(&RTCdate)) Serial.println("wrong date set!");
 }
 
 void setup() {
   M5.begin();
   M5.Power.begin();
 
-  M5.Lcd.print("Hello World on LCD");
-
-  Serial.begin(115200);
   delay(1000);
-  Serial.println("Hello World on Serial, setup");
+  setupTime();
+  M5.Lcd.setTextSize(2);  // Set the text size.
 }
 
 void loop() {
+  flushTime();
   delay(1000);
-  Serial.println("Hello World on Serial, loop");
 }
