ee8cda10696738c03418e3d40c9badcbf5793494
Yoshihiro NAKAGAWA <taku3wzr@gmail.com>
2024-02-11 17:06:07 +0900

    add test pwm
    add test pwm

---
+++ test_m5stack_core_esp32_PWM/src/main.cpp
@@ -0,0 +1,44 @@
+/* main.c
+// m5stack_hello_world
+// Reference
+// 1. 【M5Stack】基本のスケッチ例「HelloWorld」を読み解く - 趣味と学びの雑記帳
+https://nouka-it.hatenablog.com/entry/2022/02/27/133549
+// 2. Installation · m5stack/M5Stack – PlatformIO Registry
+https://registry.platformio.org/libraries/m5stack/M5Stack/installation
+// 3. M5StackGray デューティー比固定周波数可変PWM
+https://coskxlabsite.stars.ne.jp/html/for_students/M5Stack/PWMTest/PWMTest2.html
+*/
+
+#include <Arduino.h>
+#include <M5Stack.h>
+
+uint8_t PIN_PWM_OUT = 5;
+uint8_t pwm_channel = 0;
+uint8_t pwm_duty = 128; // 50% = 128/256 = 128/2^8, pwm_resolution = 8
+  
+void setup() {
+  M5.begin();
+  M5.Power.begin();
+
+  M5.Lcd.print("Hello World");
+
+  pinMode(PIN_PWM_OUT, OUTPUT);
+  //digitalWrite(pin_pwm_out, LOW);
+  //uint8_t pwm_channel = 0;
+  uint32_t pwm_frequency = 1000;
+  uint8_t pwm_resolution = 8;
+  ledcSetup(pwm_channel, pwm_frequency, pwm_resolution);
+  ledcAttachPin(PIN_PWM_OUT, pwm_channel);
+  ledcWrite(pwm_channel, pwm_duty);
+}
+
+void loop() {
+  M5.update();
+
+  if (M5.BtnA.wasPressed()) {
+    M5.Lcd.print("Button A was pressed");
+    pwm_duty = pwm_duty + 32; // +12.5%
+    M5.Lcd.printf("PWM Duty: %d\n", pwm_duty);
+    ledcWrite(pwm_channel, pwm_duty);
+  }
+}
