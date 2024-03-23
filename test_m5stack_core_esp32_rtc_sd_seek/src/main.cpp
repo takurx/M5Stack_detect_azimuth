b34f36cabe68d1b008d30b5737dd6c056e750a63
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2024-03-23 19:07:20 +0900

    csv is UTC, RTC set also UTC
    csv is UTC, RTC set also UTC

--- test_m5stack_core_esp32_rtc_sd_seek/src/main.cpp
+++ test_m5stack_core_esp32_rtc_sd_seek/src/main.cpp
@@ -172,7 +172,8 @@ void setup () {
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   // This line sets the RTC with an explicit date & time, for example to set
   // January 21, 2014 at 3am you would call:
-  //rtc.adjust(DateTime(2024, 3, 21, 9, 3, 0));
+  // set UTC time,
+  //rtc.adjust(DateTime(2024, 3, 23, 10, 0, 0));
 
   // When the RTC was stopped and stays connected to the battery, it has
   // to be restarted by clearing the STOP bit. Let's do this to ensure
