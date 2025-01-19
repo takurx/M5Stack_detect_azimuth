5f6d0984a343ae90c7b34b4ee4676acabeb1d843
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-01-19 16:41:26 +0900

    N35, push and display state on LCD and Serial
    N35, push and display state on LCD and Serial

--- N35_M5stack_button_ABC/src/main.cpp
+++ N35_M5stack_button_ABC/src/main.cpp
@@ -9,12 +9,24 @@ bool Button_A_state;
 bool Button_B_state;
 bool Button_C_state;
 
+String print_bool (bool value) {
+  if (value) {
+    return "HIGH";
+  }
+  else {
+    return "LOW";
+  }
+}
+
 void setup() {
   M5.begin();
   M5.Power.begin();
 
   Serial.begin(115200);
   Serial.println("Hello World!");
+  M5.Lcd.clear();
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("Hello World!");
 
   pinMode(Button_A_pin, OUTPUT);
   pinMode(Button_B_pin, OUTPUT);
@@ -27,6 +39,12 @@ void setup() {
   Button_A_state = false;
   Button_B_state = false;
   Button_C_state = false;
+
+  M5.Lcd.clear();
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("Button A: " + print_bool(Button_A_state));
+  M5.Lcd.println("Button B: " + print_bool(Button_B_state));
+  M5.Lcd.println("Button C: " + print_bool(Button_C_state));
 }
 
 void loop() {
@@ -38,6 +56,12 @@ void loop() {
     else {
       digitalWrite(Button_A_pin, LOW);
     }
+    Serial.println("Button A: " + print_bool(Button_A_state));
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("Button A: " + print_bool(Button_A_state));
+    M5.Lcd.println("Button B: " + print_bool(Button_B_state));
+    M5.Lcd.println("Button C: " + print_bool(Button_C_state));
   }
   
   if (M5.BtnB.wasPressed()) {
@@ -48,6 +72,12 @@ void loop() {
     else {
       digitalWrite(Button_B_pin, LOW);
     }
+    Serial.println("Button B: " + print_bool(Button_B_state));
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("Button A: " + print_bool(Button_A_state));
+    M5.Lcd.println("Button B: " + print_bool(Button_B_state));
+    M5.Lcd.println("Button C: " + print_bool(Button_C_state));
   }
   
   if (M5.BtnC.wasPressed()) {
@@ -58,6 +88,12 @@ void loop() {
     else {
       digitalWrite(Button_C_pin, LOW);
     }
+    Serial.println("Button C: " + print_bool(Button_C_state));
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("Button A: " + print_bool(Button_A_state));
+    M5.Lcd.println("Button B: " + print_bool(Button_B_state));
+    M5.Lcd.println("Button C: " + print_bool(Button_C_state));
   }
 
 }
