b6aed102c9b227da291d126feb3502f9c9e0e089
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-01-19 15:28:01 +0900

    N35, Button ABC push
    N35, Button ABC push

--- N35_M5stack_button_ABC/src/main.cpp
+++ N35_M5stack_button_ABC/src/main.cpp
@@ -1,21 +1,63 @@
 #include <Arduino.h>
+#include <M5Stack.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int Button_A_pin = 2;
+int Button_B_pin = 5;
+int Button_C_pin = 13;
+
+bool Button_A_state;
+bool Button_B_state;
+bool Button_C_state;
 
 void setup() {
- Serial.begin(115200);
- pinMode(13, OUTPUT);
-  
-  Serial.println(result);
+  M5.begin();
+  M5.Power.begin();
+
+  Serial.begin(115200);
   Serial.println("Hello World!");
+
+  pinMode(Button_A_pin, OUTPUT);
+  pinMode(Button_B_pin, OUTPUT);
+  pinMode(Button_C_pin, OUTPUT);
+
+  digitalWrite(Button_A_pin, LOW);
+  digitalWrite(Button_B_pin, LOW);
+  digitalWrite(Button_C_pin, LOW);
+
+  Button_A_state = false;
+  Button_B_state = false;
+  Button_C_state = false;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (M5.BtnA.wasPressed()) {
+    Button_A_state = !Button_A_state;
+    if (Button_A_state) {
+      digitalWrite(Button_A_pin, HIGH);
+    }
+    else {
+      digitalWrite(Button_A_pin, LOW);
+    }
+  }
+  
+  if (M5.BtnB.wasPressed()) {
+    Button_B_state = !Button_B_state;
+    if (Button_B_state) {
+      digitalWrite(Button_B_pin, HIGH);
+    }
+    else {
+      digitalWrite(Button_B_pin, LOW);
+    }
+  }
+  
+  if (M5.BtnC.wasPressed()) {
+    Button_C_state = !Button_C_state;
+    if (Button_C_state) {
+      digitalWrite(Button_C_pin, HIGH);
+    }
+    else {
+      digitalWrite(Button_C_pin, LOW);
+    }
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
